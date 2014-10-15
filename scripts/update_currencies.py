89075b2d977baa371c97f3dd8d074e1d655aa5e6
gabi <gabi@kazav.net>
2014-10-15 22:56:32 +0300

    currencies

--- scripts/update_currencies.py
+++ scripts/update_currencies.py
@@ -8,7 +8,7 @@ curs= [
 
 for cur in curs:
  r=requests.get('http://fx-rate.net/'+str(cur)+'/ils')
- rate= float(r.text[r.text.find('<div style="font-size:25px;color:black;margin-top:5px">'):r.text.find('<div style="font-size:25px;color:black;margin-top:5px">')+80].split('>')[1].split()[0])
- file = open("currencies/"+cur, "w")
+ rate=(r.text[r.text.find('Today = ')+8:r.text.find('Today = ')+13])
+ file = open("../currencies/"+cur, "w")
  file.write(str(rate))
  file.close
