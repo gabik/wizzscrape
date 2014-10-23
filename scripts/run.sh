839d947c7ca067426dc291aea5e30c28613dbeb8
gabi <gabi@kazav.net>
2014-10-23 12:44:07 +0300

    parallel scrapers sorting

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
 ./check_last_run.sh
-for y in wizz easyjet up airmed elal vueling norwegian ; do
+for y in `cat SM$SM/companies` ; do
  for i in `./get_dst.sh $y` ; do unbuffer python ../scrapers/${y}_scrape.py $i $1 $2 &> ../logs/${y}_$i.log & sleep 20 ;  done
  sleep 50
 done
