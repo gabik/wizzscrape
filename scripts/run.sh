4849afb391ee36075a671122e72e625aabed8ac4
From SM2 <ec2-user@SM2.localdomain>
2014-10-23 13:49:42 +0300

    fix run

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,5 @@
 ./check_last_run.sh
+SM=`cat ~/SM`
 for y in `cat SM$SM/companies` ; do
  for i in `./get_dst.sh $y` ; do unbuffer python ../scrapers/${y}_scrape.py $i $1 $2 &> ../logs/${y}_$i.log & sleep 20 ;  done
  sleep 50
