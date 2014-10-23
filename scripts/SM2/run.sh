fcb0b815871a4845cdc19ff0bebbbf758b7783d8
From SM2 <ec2-user@ip-10-0-0-239.ec2.internal>
2014-10-23 12:33:12 +0300

    2 scrapers

--- scripts/SM2/run.sh
+++ scripts/SM2/run.sh
@@ -1,6 +1,6 @@
 cd ..
 ./check_last_run.sh
 for y in airmed elal vueling norwegian ; do
- for i in `./get_dst.sh $y` ; do unbuffer python ../scrapers/${y}_scrape.py $i $1 $2 &> ../logs/${y}_$i.log & sleep 20 ;  done
+ for i in `./get_dst.sh $y` ; do unbuffer python ../scrapers/${y}_scrape.py $i $1 $2 &> ../logs/${y}_$i.log & sleep 45 ;  done
  sleep 50
 done
