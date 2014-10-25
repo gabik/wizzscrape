4e1cf9d7f2acbc195267354a7c7f58b4dddfe506
EC2 Default User <ec2-user@SM1.localdomain>
2014-10-26 00:03:58 +0300

    fix check script + cur

--- scripts/check_last_run.sh
+++ scripts/check_last_run.sh
@@ -3,9 +3,11 @@ cd ../logs
 q=$(mktemp)
 for i in *.log ; do if ! grep Done $i &> /dev/null ; then echo ${i}-Failed ; cat $i ; echo '--------------------------------------------';  fi ; done > $q
 if [[ $(wc -l $q | awk '{print $1}') -ne 0 ]] ; then
+q1=$(mktemp)
+sed 's///g' $q > $q1
 new_logs=`date +%d%m%H%M`
  mkdir $new_logs
  cp *.log $new_logs
- mail -r "ScrapersManager@2fly.cheap" -s "Scrapers Failed - `hostname` `date`" 2flycheap@kazav.net < $q
+ mail -r "ScrapersManager@2fly.cheap" -s "Scrapers Failed - `hostname` `date`" 2flycheap@kazav.net < $q1
 fi
-#rm -f $q
+rm -f $q $q1
