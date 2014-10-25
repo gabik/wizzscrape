34efe97fbad21f1a34dd77aeb0f17297da9fa585
EC2 Default User <ec2-user@SM1.localdomain>
2014-10-25 23:53:07 +0300

    fail mail heder

--- scripts/check_last_run.sh
+++ scripts/check_last_run.sh
@@ -6,6 +6,6 @@ if [[ $(wc -l $q | awk '{print $1}') -ne 0 ]] ; then
 new_logs=`date +%d%m%H%M`
  mkdir $new_logs
  cp *.log $new_logs
- mail -r "ScrapersManager@2fly.cheap" -s "Scrapers Failed - `date`" 2flycheap@kazav.net < $q
+ mail -r "ScrapersManager@2fly.cheap" -s "Scrapers Failed - `hostname` `date`" 2flycheap@kazav.net < $q
 fi
-rm -f $q
+#rm -f $q
