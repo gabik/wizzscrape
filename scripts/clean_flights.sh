ce0b0b7ad7088fc14a3d51e2ac1886ce29502681
EC2 Default User <ec2-user@SM1.localdomain>
2014-10-26 09:23:04 +0200

    daily status

--- scripts/clean_flights.sh
+++ scripts/clean_flights.sh
@@ -3,4 +3,14 @@ psql -t -h$DB_HOST -Uroot -w GabiScrape << EOF
 delete from flights where date<(now() - '0 day'::INTERVAL) ;
 EOF
 
+today=`date +%Y-%m-%d`
+q=`mktemp`
+psql -t -h$DB_HOST -Uroot -w GabiScrape << EOF > $q
+select * from flights where scrape_time<'$today' order by scrape_time
+EOF
+q1=$(mktemp)
+sed 's/^M//g' $q > $q1
+new_logs=`date +%d%m%H%M`
+mail -r "ScrapersManager@2fly.cheap" -s "Beta daily status - `date`" 2flycheap@kazav.net < $q1
+rm -f $q $q1
 
