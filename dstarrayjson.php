aceb4fe4bc51db45173c41690df88a1f8fb1d986
EC2 Default User <ec2-user@Web1.localhost>
2014-11-09 19:57:17 +0200

    array dst

---
+++ dstarrayjson.php
@@ -0,0 +1,18 @@
+
+<?php
+include 'db_con_string.php';
+$db = pg_pconnect($conn_string);
+
+$query="select string_agg(cast(a.id as text), ','), airport  from destinations f join companies a on f.company=a.name group by airport";
+$result = pg_query($db, $query);
+
+pg_close();
+
+
+$json=array();
+while ($row = pg_fetch_row($result)) {
+ $cur_elem=array('airport' => $row[1], 'companies' => "[$row[0]]");
+ array_push($json, $cur_elem);
+}
+echo json_encode($json);
+?>
