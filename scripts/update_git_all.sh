8db9ac9e48f059109356d596b93226f8fc0ab63d
EC2 Default User <ec2-user@SM1.localhost>
2014-11-09 19:00:00 +0200

    small fix

--- scripts/update_git_all.sh
+++ scripts/update_git_all.sh
@@ -1,1 +1,1 @@
-sh sm1.2fly.cheap "cd wizz ; git pull" ; ssh sm2.2fly.cheap "cd wizz ; git pull" ; ssh 2fly.cheap "cd wizz ; git pull"
+ssh sm1.2fly.cheap "cd wizz ; git pull" ; ssh sm2.2fly.cheap "cd wizz ; git pull" ; ssh 2fly.cheap "cd wizz ; git pull"
