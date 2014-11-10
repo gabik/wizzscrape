3b0c914eca4c0f5eb5d41c3fc16018897505665a
EC2 Default User <ec2-user@Web1.localhost>
2014-11-10 10:03:47 +0200

    add dst name to dstarray

--- dstarrayjson.php
+++ dstarrayjson.php
@@ -3,7 +3,7 @@
 include 'db_con_string.php';
 $db = pg_pconnect($conn_string);
 
-$query="select string_agg(cast(a.id as text), ','), airport  from destinations f join companies a on f.company=a.name group by airport";
+$query="select string_agg(cast(a.id as text), ','), airport, max(destination)  from destinations f join companies a on f.company=a.name group by airport";
 $result = pg_query($db, $query);
 
 pg_close();
@@ -11,7 +11,7 @@ pg_close();
 
 $json=array();
 while ($row = pg_fetch_row($result)) {
- $cur_elem=array('airport' => $row[1], 'companies' => "[$row[0]]");
+ $cur_elem=array('airport' => $row[1], 'companies' => "[$row[0]]", 'destination' => $row[2]);
  array_push($json, $cur_elem);
 }
 echo json_encode($json);
