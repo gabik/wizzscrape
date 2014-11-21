9b0b09bea082fbf7391b6bb424636e4fe6018566
EC2 Default User <ec2-user@SM.2fly.cheap>
2014-11-21 11:52:25 +0200

    start using spot

---
+++ scripts/boot_run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd ~/wizz
+git pull
+cd ~/wizz/scripts
+./update_currencies.py > ../logs/cur.log
+
+if [[ $# -le 3 ]] ; then
+ echo Missing arguments. 
+ echo Usage $0 Machine DST Month [debug]
+fi
+
+echo $1 > ~/SM
+sudo hostname SM${1}.2fly.cheap
+shift
+./run.sh $*
+
+count_timeout=0
+run_timeout=20
+# check if something still runs
+while [[ $(ps -ef | grep scrape | grep -v grep | wc -l) -ne 0 ]] ; do
+ sleep 60
+ count_timeout=$((count_timeout+1))
+ if [[ count_timeout -ge run_timeout ]] ; then
+  break
+ fi
+done
+
+./check_last_run.sh 
+
+sudo poweroff
--- scripts/check_last_run.sh
+++ scripts/check_last_run.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
+
 cd ../logs
+
+SM=$(cat ~/SM)
 q=$(mktemp)
-for i in *.log ; do if ! grep Done $i &> /dev/null ; then echo ${i}-Failed ; cat $i ; echo '--------------------------------------------';  fi ; done > $q
+
+for cmp in $(cat ../scripts/SM${SM}/companies) ; do 
+ for dst in $(../scripts/get_dst.sh $cmp) ; do
+  logfile=${cmp}_${dst}.log
+  if ! grep Done $logfile &> /dev/null ; then 
+   echo ${logfile} -Failed 
+   cat $logfile
+   echo '--------------------------------------------'
+  fi
+ done
+done > $q
+
 if [[ $(wc -l $q | awk '{print $1}') -ne 0 ]] ; then
-q1=$(mktemp)
-sed 's///g' $q > $q1
-new_logs=`date +%d%m%H%M`
- mkdir $new_logs
- cp *.log $new_logs
+ q1=$(mktemp)
+ sed 's///g' $q > $q1
+ #new_logs=`date +%d%m%H%M`
+ #mkdir $new_logs
+ #cp *.log $new_logs
  mail -r "ScrapersManager@2fly.cheap" -s "Scrapers Failed - `hostname` `date`" 2flycheap@kazav.net < $q1
 fi
 rm -f $q $q1
