ecf86840e6cb49bd61f16ade33d73610bdde402f
EC2 Default User <ec2-user@SM-host.2fly.cheap>
2014-12-27 17:26:49 +0200

    test shuffle run array

--- scripts/run.py
+++ scripts/run.py
@@ -9,9 +9,10 @@ import sys
 
 max_sleep = 20
 
-argv_month=sys.argv[1]
+arg_month=sys.argv[1]
 debug_flag = "" if len(sys.argv) < 3 else "debug"
 time.sleep(randint(0,max_sleep*60))
+all_runs = []
 db= psycopg2.connect( host="gabiscrape.c8f6qy9d6xm4.us-west-2.rds.amazonaws.com", database="GabiScrape", user="root", password="ManegerDB")
 SM=open(os.path.expanduser('~/SM')).readline().strip()
 companies = open('SM{0}/companies'.format(SM)).readlines()
@@ -23,8 +24,12 @@ for c in companies:
 	dsts = cur1.fetchall()
 	shuffle(dsts)
 	for d in dsts:
-		subprocess.call('unbuffer python ../scrapers/{0}_scrape.py {1} {2} {3} &> ../logs/{0}_{1}.log &'.format(cur_c, d[0], sys.argv[1], debug_flag), shell=True)
-		time.sleep(30)
+		all_runs.append({'dst': d[0], 'cmp': cur_c})
+
+shuffle(all_runs)
+for r in all_runs:
+	subprocess.call('unbuffer python ../scrapers/{0}_scrape.py {1} {2} {3} &> ../logs/{0}_{1}.log'.format(r['cmp'], r['dst'], arg_month, debug_flag), shell=True)
+	time.sleep(10)
 
 finish_file = open('/tmp/finished_to_run.scrape', 'w')
 finish_file.write('1')
