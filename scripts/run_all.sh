7874d33a0e84d8b9aa6aaad908381d718a005c79
EC2 Default User <ec2-user@SM-host.2fly.cheap>
2015-01-08 14:07:01 +0200

    run all fix

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,2 +1,14 @@
-for i in {0..12} ; do ./run.py $i &  done
-watch "ps -ef | grep run.py | grep -v grep | grep -v watch ; ps -ef | grep python | grep expect | grep -v watch | awk '{print  \$12, \$14}' | sort | uniq "
+if [ $# -ne 2 ] ; then
+ echo run_all.sh START STOP
+ exit 1
+fi
+
+echo 'watch "ps -ef | grep run.py | grep -v grep | grep -v watch ; ps -ef | grep python | grep expect | grep -v watch | awk '"'"'{print  \$12, \$13, \$14}'"'"' | sort"'
+for i in $(eval echo {$1..$2}) ; do 
+ ./run.py $i debug &
+ sleep 2
+ while ps -ef | egrep "run.py|expect" | grep -v grep | grep -v watch &> /dev/null ; do
+  echo waiting
+  sleep 10
+ done
+done
